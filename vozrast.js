cd5781f0ccf1eee7bfdd8121ecc64675c48950c7
Artem Karasev <art.karasev@gmail.com>
2023-08-15 23:46:27 -0700

    refactor function with naming

--- vozrast.js
+++ vozrast.js
@@ -1,2 +1,14 @@
-let age = prompt('Введите ваш год рождения');
-alert('Вам '+ (2023 - age) + ' лет');
+function getAgeWord(age) {
+    if (age % 10 === 1 && age % 100 !== 11) {
+        return 'год';
+    } else if (age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
+        return 'года';
+    } else {
+        return 'лет';
+    }
+}
+
+const getAge = prompt('Введите ваш год рождения');
+const result = 2023 - getAge;
+
+alert(`Вам ${result} ${getAgeWord(result)}`);
